be8ac1e6eb6e35129c029818cda48a503aa135ac
siddharthX6174 <siddharth_x6174@outlook.com>
2025-12-27 12:54:33 +0530

    .

--- BEPO/Pair/Pair.cpp
+++ BEPO/Pair/Pair.cpp
@@ -1,27 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+template <typename T1, typename T2>
 class Pair1 {
 public:
-	int first;
-	int second;
+	T1 first;
+	T2 second;
 
-	Pair1() {
-		first = 0;
-		second = 0;
-	}
+	Pair1() {}  // default constructor (IMPORTANT)
 
-	Pair1(int first, int second) {
+	Pair1(T1 first, T2 second) {
 		this->first = first;
 		this->second = second;
 	}
 };
 
 int main() {
-	Pair1 yo;
+	Pair1<int, int> yo;
 
 	yo.first = 100;
 	yo.second = 200;
 
-	cout << yo.first << " " << yo.second;
+	cout << yo.first << " " << yo.second << endl;
+
+	// array of pairs
+	Pair1<string, int> arr[10];
+
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> arr[i].first >> arr[i].second;
+	}
+
+	for (int i = 0; i < t; i++) {
+		cout << arr[i].first << " -> " << arr[i].second << endl;
+	}
+
+	return 0;
 }
