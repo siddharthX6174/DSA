15145d98fc826377d1e378f638dca1850b2253ed
siddharthX6174 <siddharth_x6174@outlook.com>
2026-01-15 18:06:42 +0530

    ..

--- BEPO/Number Theory/Modulo_Properties.cpp
+++ BEPO/Number Theory/Modulo_Properties.cpp
@@ -1,8 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int N = 1e5 + 10;
-const int P = 1e9 + 10;
+const int P = 1e9 + 7;
 #define int long long
+int Fact[N] {0};
 
 // Time Complexity: O(1)
 // Addition Property of modulo:
@@ -55,9 +56,51 @@ int FastPowerV2(int x, int y) {
 
 // (x/y)%P
 /*
+	Inverse of y with respect to p:
 
+	(y^-1) % P:
+
+	According to FLT: (y^P-2) % P
 */
+// O(log y)
+int Inverse(int y) {
+	return FastPowerV2(y, P - 2);
+}
 
+
+// Based on Fermat's little theorem...
+int Division(int x, int y) {
+	return (Multiply(x, Inverse(y)));
+}
+
+
+// O(n) : Large Factorial->....
+void calculate_Factorial() {
+	Fact[0] = 1;
+	for (int i = 1; i < N; i++) {
+		Fact[i] = Multiply(Fact[i - 1], i);
+	}
+}
+
+//O(log n)
+int ncr(int n, int r) {
+	// ncr = n! / (r!*(n-r)!)
+
+	return Multiply( Multiply(Fact[n], Inverse(Fact[r])), Inverse(Fact[n - r]));
+}
+
+// n p r
+int npr(int n, int r) {
+	/*
+		npr = n! / (n-r)!;
+	*/
+	return Multiply(Fact[n], Inverse(Fact[n - r]));
+}
 int32_t main() {
+
 	cout << FastPowerV2(3, 8) << endl;
+	calculate_Factorial();
+
+	cout << npr(1000, 500) << endl;
+	return 0;
 }
